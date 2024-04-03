3b6e90d76d06ca52563565f0fbb4e4e0b9f0c018
warrdev <warrdev08@gmail.com>
2024-04-03 01:35:38 +0000

    changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Profile;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Str;
 
 class AuthController extends Controller
@@ -18,16 +20,17 @@ class AuthController extends Controller
             'password' => 'required'
         ]);
         $user = User::where("username", $request->username)->first();
-        if(!$user || !Hash::check($request->password, $user->password)) {
+        if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'username' => ['The provided credentials are incorrect.'],
             ]);
         }
-        if($user->status!='verified'){
+        if ($user->status != 'verified') {
             return \response([
-                'status'=>'failed',
-                'msg'=> 'Account was not verified yet'
-            ],403);
+                'status' => 'failed',
+                'message' => 'Account was not verified yet',
+                'shouldValidate' => true
+            ], 403);
         }
         return response()->json(['user' => $user, 'token' => $user->createToken($user->username)->plainTextToken]);
     }
@@ -36,7 +39,7 @@ class AuthController extends Controller
     {
         $validatedData = $request->validate([
             'fname' => 'required|string|max:255',
-            'mname' => 'required|string|max:255',
+            'mname' => 'string|max:255',
             'lname' => 'required|string|max:255',
             'username' => 'required|string|max:255|unique:users',
             'cpnum' => 'required|digits:11',
@@ -45,21 +48,23 @@ class AuthController extends Controller
         ]);
 
         $user = User::create([
-                'username' => $validatedData['username'],
-                'cpnum' => $validatedData['cpnum'],
-                'email' => $validatedData['email'],
-                'password' => Hash::make($validatedData['password']),
-                'code' => Str::random(4),
+            'username' => $validatedData['username'],
+            'cpnum' => $validatedData['cpnum'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            'code' => Str::random(4),
+        ]);
+        if ($user)
+            Profile::create([
+                'fname' => $validatedData['fname'],
+                'mname' => $validatedData['mname'],
+                'lname' => $validatedData['lname'],
+                'user_id' => $user->id,
             ]);
-            if($user)
-                Profile::create([
-                    'fname' => $validatedData['fname'],
-                    'mname' => $validatedData['mname'],
-                    'lname' => $validatedData['lname'],
-                    'user_id' => $user->id,
-                ]);
-        $ms="Triser: your code is ".$user->code;
-        $asd=$this->itexmo($user->cpnum,$ms);
+        $ms = "Triser: your code is " . $user->code;
+        // $asd = $this->itexmo($user->cpnum, $ms);
+
+        // $mail = new MailMessage();
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
@@ -79,14 +84,14 @@ class AuthController extends Controller
     public function logout()
     {
         $request->user()->currentAccessToken()->delete();
-        return \response(['status'=>'successful','msg'=>'Log out successful']);
+        return \response(['status' => 'successful', 'msg' => 'Log out successful']);
     }
     public function myAccount()
     {
         return \response([
-            'status'=>'successful',
-            'user'=> auth('sanctum')->user(),
-            'profile'=> auth('sanctum')->user()->profile
+            'status' => 'successful',
+            'user' => auth('sanctum')->user(),
+            'profile' => auth('sanctum')->user()->profile
         ]);
     }
     public function verify(Request $request)
@@ -94,71 +99,46 @@ class AuthController extends Controller
         $validatedData = $request->validate([
             'code' => 'required',
         ]);
-        $user=User::where('code',$request->input('code'))->first();
-        if(!$user){
+        $user = User::where('code', $request->input('code'))->first();
+        if (!$user) {
             return \response([
-                'status'=>'failed',
-                'msg'=> 'Code was not valid'
-            ],403);
+                'status' => 'failed',
+                'msg' => 'Code was not valid'
+            ], 403);
         }
-        $user->status='verified';
+        $user->status = 'verified';
         $user->save();
         return \response([
-            'status'=>'success',
-            'msg'=> 'The account was verified successfully'
+            'status' => 'success',
+            'msg' => 'The account was verified successfully'
         ]);
     }
     public function update(Request $request)
     {
-        $user=auth('sanctum')->user();
+        $user = auth('sanctum')->user();
         // return response($user->username);
         $validatedData = $request->validate([
-            'username' =>'required|string',
+            'username' => 'required|string',
             'cpnum' => 'required|digits:11',
             'email' => 'required|email',
         ]);
-        $data=[
-            'username'=>$request->input('username'),
-            'cpnum'=>$request->input('cpnum'),
-            'email'=>$request->input('email'),
+        $data = [
+            'username' => $request->input('username'),
+            'cpnum' => $request->input('cpnum'),
+            'email' => $request->input('email'),
         ];
-        if($request->input('password'))
-            $data['password']=$request->input('password');
+        if ($request->input('password'))
+            $data['password'] = $request->input('password');
         $user->update($data);
-        if($user->acctype!='admin')
+        if ($user->acctype != 'admin')
             $user->profile->update([
-                'fname'=>$request->input('fname'),
-                'mname'=>$request->input('mname'),
-                'lname'=>$request->input('lname')
-                ]);
+                'fname' => $request->input('fname'),
+                'mname' => $request->input('mname'),
+                'lname' => $request->input('lname')
+            ]);
         return \response([
-            'status'=>'successful',
-            'msg'=> "Profile Updated Successfully",
+            'status' => 'successful',
+            'msg' => "Profile Updated Successfully",
         ]);
-    }
-    
-    //##########################################################################
-    // ITEXMO SEND SMS API - PHP - CURL-LESS METHOD
-    // Visit www.itexmo.com/developers.php for more info about this API
-    //##########################################################################
-    function itexmo($number,$message){
-        $apicode=config('app.apiCode');
-        $passwd=config('app.apiPass');
-        $url = 'https://www.itexmo.com/php_api/api.php';
-        $itexmo = array(
-            '1' => $number, 
-            '2' => $message.'
-', 
-            '3' => $apicode, 
-            'passwd' => $passwd);
-        $param = array(
-            'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'POST',
-                'content' => http_build_query($itexmo),
-            ),
-        );
-        $context  = stream_context_create($param);
-        return file_get_contents($url, false, $context);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,33 +15,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('', function (Request $request): JsonResponse {
+    return response()->json([
+        'message' => 'ok'
+    ]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return App\Models\User::all();
 });
 Route::middleware('auth:sanctum')->get('/checksession', function (Request $request) {
-    return response(['status'=>'ok']);
+    return response(['status' => 'ok']);
 });
 
-Route::post('/register', ['uses'=> '\App\Http\Controllers\AuthController@register']);
-Route::post('/login', ['uses'=> '\App\Http\Controllers\AuthController@login']);
-Route::post('/verify', ['uses'=> '\App\Http\Controllers\AuthController@verify']);
+Route::post('/register', ['uses' => '\App\Http\Controllers\AuthController@register']);
+Route::post('/login', ['uses' => '\App\Http\Controllers\AuthController@login']);
+Route::post('/verify', ['uses' => '\App\Http\Controllers\AuthController@verify']);
 // Route::get('/test/{id}', ['uses'=> '\App\Http\Controllers\TricycleController@show']);
 
-Route::get('/drivers', ['uses'=> '\App\Http\Controllers\TricycleController@index']);
+Route::get('/drivers', ['uses' => '\App\Http\Controllers\TricycleController@index']);
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/assign/{id}', ['uses'=> '\App\Http\Controllers\TransactionsController@assign']);
-    Route::get('/transaction/{id}/cancel', ['uses'=> '\App\Http\Controllers\TransactionsController@cancel']);
-    Route::post('/adddriver', ['uses'=> '\App\Http\Controllers\TricycleController@create']);
-    Route::post('/service', ['uses'=> '\App\Http\Controllers\TransactionsController@store']);
-    Route::post('/pickup', ['uses'=> '\App\Http\Controllers\BookingController@pickup']);
-    Route::get('/transactions', ['uses'=> '\App\Http\Controllers\TransactionsController@transactions']);
-    Route::delete('/driver/{id}', ['uses'=> '\App\Http\Controllers\TricycleController@destroy']);
-    Route::get('/driver/{id}', ['uses'=> '\App\Http\Controllers\TricycleController@show']);
-    Route::patch('/driver/{id}', ['uses'=> '\App\Http\Controllers\TricycleController@update']);
-    Route::patch('/driver/{id}/ban', ['uses'=> '\App\Http\Controllers\TricycleController@bandriver']);
-    Route::patch('/driver/{id}/unban', ['uses'=> '\App\Http\Controllers\TricycleController@unbandriver']);
-    Route::patch('/rateservice/{id}', ['uses'=> '\App\Http\Controllers\TransactionsController@rate']);
-    Route::get('/myaccount', ['uses'=> '\App\Http\Controllers\AuthController@myAccount']);
-    Route::patch('/myaccount', ['uses'=> '\App\Http\Controllers\AuthController@update']);
-    Route::get('/getlastlocation', ['uses'=> '\App\Http\Controllers\TransactionsController@getlastlocation']);
+    Route::post('/assign/{id}', ['uses' => '\App\Http\Controllers\TransactionsController@assign']);
+    Route::get('/transaction/{id}/cancel', ['uses' => '\App\Http\Controllers\TransactionsController@cancel']);
+    Route::post('/adddriver', ['uses' => '\App\Http\Controllers\TricycleController@create']);
+    Route::post('/service', ['uses' => '\App\Http\Controllers\TransactionsController@store']);
+    Route::post('/pickup', ['uses' => '\App\Http\Controllers\BookingController@pickup']);
+    Route::get('/transactions', ['uses' => '\App\Http\Controllers\TransactionsController@transactions']);
+    Route::delete('/driver/{id}', ['uses' => '\App\Http\Controllers\TricycleController@destroy']);
+    Route::get('/driver/{id}', ['uses' => '\App\Http\Controllers\TricycleController@show']);
+    Route::patch('/driver/{id}', ['uses' => '\App\Http\Controllers\TricycleController@update']);
+    Route::patch('/driver/{id}/ban', ['uses' => '\App\Http\Controllers\TricycleController@bandriver']);
+    Route::patch('/driver/{id}/unban', ['uses' => '\App\Http\Controllers\TricycleController@unbandriver']);
+    Route::patch('/rateservice/{id}', ['uses' => '\App\Http\Controllers\TransactionsController@rate']);
+    Route::get('/myaccount', ['uses' => '\App\Http\Controllers\AuthController@myAccount']);
+    Route::patch('/myaccount', ['uses' => '\App\Http\Controllers\AuthController@update']);
+    Route::get('/getlastlocation', ['uses' => '\App\Http\Controllers\TransactionsController@getlastlocation']);
 });
